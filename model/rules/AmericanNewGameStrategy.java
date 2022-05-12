ed57e98ffb642a01167997cb76046027e64c22c9
Jonathan Berg (jb223pt) <jb223pt@student.lnu.se>
2022-05-12 14:38:52 +0200

    Add files via upload

---
+++ model/rules/AmericanNewGameStrategy.java
@@ -0,0 +1,22 @@
+package BlackJack.model.rules;
+
+import BlackJack.model.Deck;
+import BlackJack.model.Dealer;
+import BlackJack.model.Player;
+import BlackJack.model.Card;  
+
+class AmericanNewGameStrategy implements INewGameStrategy {
+
+  public boolean NewGame(Dealer a_dealer, Player a_player) {
+
+    a_dealer.giveCard(a_player, true);
+
+    a_dealer.giveCard(a_dealer, true);
+
+    a_dealer.giveCard(a_player, true);
+
+    a_dealer.giveCard(a_dealer, false);
+
+    return true;
+  }
+}
---
+++ view/SimpleView.java
@@ -0,0 +1,74 @@
+package BlackJack.view;
+
+import java.io.IOException;
+import java.util.EnumSet;
+import java.util.Scanner;
+
+public class SimpleView implements IView 
+{
+
+  public void DisplayWelcomeMessage()
+        {
+          for(int i = 0; i < 50; i++) {System.out.print("\n");}; 
+          System.out.println("Hello Black Jack World");
+          //System.out.println("Type 'p' to Play, 'h' to Hit, 's' to Stand or 'q' to Quit\n");
+        }
+
+        public Choices GetInput(){
+        	int choice = 0;  
+        	Scanner scan = new Scanner(System.in); 
+                for (Choices choices : EnumSet.allOf(Choices.class)) {
+                   System.out.print(choices.ordinal()+1+". "+choices + "   ");
+                  }
+                System.out.println("");
+                try{ 
+                	choice = scan.nextInt();	
+                }catch(Exception e){
+                	//e.printStackTrace();
+                	System.out.println("Wrong input, Quitting..");
+                	return Choices.values()[2]; 
+                }
+                 
+               return Choices.values()[choice-1];
+            }
+
+        public void DisplayCard(BlackJack.model.Card a_card)
+        {
+            System.out.println("" + a_card.GetValue() + " of " + a_card.GetColor());
+        }
+
+        public void DisplayPlayerHand(Iterable<BlackJack.model.Card> a_hand, int a_score)
+        {
+            DisplayHand("Player", a_hand, a_score);
+        }
+
+        public void DisplayDealerHand(Iterable<BlackJack.model.Card> a_hand, int a_score)
+        {
+            DisplayHand("Dealer", a_hand, a_score);
+        }
+
+        private void DisplayHand(String a_name, Iterable<BlackJack.model.Card> a_hand, int a_score)
+        {
+            System.out.println(a_name + " Has: ");
+            for(BlackJack.model.Card c : a_hand)
+            {
+                DisplayCard(c);
+            }
+            System.out.println("Score: " + a_score);
+            System.out.println("");
+        }
+
+        public void DisplayGameOver(boolean a_dealerIsWinner)
+        {
+            System.out.println("GameOver: ");
+            if (a_dealerIsWinner)
+            {
+                System.out.println("Dealer Won!");
+            }
+            else
+            {
+                System.out.println("You Won!");
+            }
+            
+        }
+    }
